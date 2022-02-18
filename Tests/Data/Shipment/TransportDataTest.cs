99ee55488ca8936850b44ec7524496b204e69e59
Anna Brita Ani <annani@ttu.ee>
2022-02-18 16:24:57 +0200

    Anna

---
+++ Tests/Data/Shipment/TransportDataTest.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lana_jewelry.Data.Shipment;
+
+namespace Lana_jewelry.Tests.Data.Shipment
+{
+    [TestClass]
+    public class TransportDataTest : BaseTests<TransportData>{
+        [TestMethod] public void TransportIdTest() => isProperty<string>();
+        [TestMethod] public void CostumerAddressTest() => isProperty<string>();
+        [TestMethod] public void TransportPriceTest() => isProperty<double>();
+        [TestMethod] public void TransportDurationTest() => isProperty<DateTime>();
+    }
+}
---
+++ Tests/Facade/Shipment/TraansportViewTests.cs
@@ -0,0 +1,18 @@
+﻿using Lana_jewelry.Facade.Shipment;
+using Lana_jewelry.Tests.Data.Shipment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lana_jewelry.Tests.Facade.Shipment
+{
+    [TestClass]
+    public class TraansportViewTests:BaseTests<TransportView>
+    {
+        [TestMethod] public void TransportIdTest() => isProperty<string>();
+        [TestMethod] public void CostumerAddressTest() => isProperty<string>();
+        [TestMethod] public void TransportPriceTest() => isProperty<double>();
+        [TestMethod] public void TransportDurationTest() => isProperty<DateTime>();
+    }
+}
