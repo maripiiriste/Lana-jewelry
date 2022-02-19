a09396a691e5feee5ec8abba2da8c2f6c851a146
Anna Brita Ani <annani@ttu.ee>
2022-02-19 16:02:21 +0200

    Anna

--- Pages/Extensions/MyEditorForHtml.cs
+++ Pages/Extensions/MyEditorForHtml.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
 
 
