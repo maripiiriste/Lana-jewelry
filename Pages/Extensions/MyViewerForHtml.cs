cca07cd1ca4d6ac0e643edc2c32b4112793dbdb0
Mari Piiriste <mpiiri@ttu.ee>
2022-02-21 22:24:40 +0200

    Mari

--- Pages/Extensions/MyViewerForHtml.cs
+++ Pages/Extensions/MyViewerForHtml.cs
@@ -15,31 +15,8 @@ namespace Lana_jewelry.Pages.Extensions
 
         private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
         {
-            var l = new List<object>();
-            //l.Add(new HtmlString("<div class=\"row\">"));
-            //l.Add(new HtmlString("<dd class=\"col-sm-2\">"));
-            //l.Add(html.LabelFor(expression, null, new { @class = "control-label" }));
-            //l.Add(new HtmlString("</dd>"));
-            //l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
-            //l.Add(html.EditorFor(expression, new { htmlAttributes = new { @class = "fore-control" } }));
-            //l.Add(html.ValidationMessageFor(expression, null, new { @class = "text-danger" }));
-            //l.Add(new HtmlString("</dd>"));
-            //l.Add(new HtmlString("</div>"));
-
-        
-         l.Add(new HtmlString("<dl class=\"row\">"));
-        l.Add(new HtmlString("<dt class=\"col-sm-2\">"));
-        l.Add(h.DisplayNameFor(e));
-        l.Add(new HtmlString("</dt>"));
-        l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
-        l.Add(h.DisplayFor(e));
-        l.Add(new HtmlString("</dd>"));
-        l.Add(new HtmlString("<dt class=\"col-sm-2\">"));
-        l.Add(h.DisplayNameFor(e));
-        l.Add(new HtmlString("</dt>"));
-        l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
-        l.Add(h.DisplayFor(e));
-        l.Add(new HtmlString("</dd>"));
+        var l = new List<object>();
+        l.Add(new HtmlString("<dl class=\"row\">"));
         l.Add(new HtmlString("<dt class=\"col-sm-2\">"));
         l.Add(h.DisplayNameFor(e));
         l.Add(new HtmlString("</dt>"));
@@ -47,7 +24,7 @@ namespace Lana_jewelry.Pages.Extensions
         l.Add(h.DisplayFor(e));
         l.Add(new HtmlString("</dd>"));
         l.Add(new HtmlString("</dl>"));
-          
+
             return l;
 
         } 
