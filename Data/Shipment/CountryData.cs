7e166bcd5f4e86ef8f8ce80e09d7b8792d95bee8
Anna Brita Ani <annani@ttu.ee>
2022-03-24 08:54:41 +0200

    anna

---
+++ Data/Shipment/CountryData.cs
@@ -0,0 +1,11 @@
+﻿namespace Lana_jewelry.Data.Shipment
+{
+    public class CountryData: EntityData{
+        public string Code { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+    public class CurrencyData : EntityData
+    {
+    }
+}
---
+++ Infra/Initializers/CountriesInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Lana_jewelry.Data.Shipment;
+using System.Globalization;
+
+namespace Lana_jewelry.Infra.Initializers
+{
+    public sealed class CountriesInitializer : BaseInitializer<CountryData>
+    {
+        public CountriesInitializer(Lana_jewelryDb? db): base(db, db?.Countries) { }
+        protected override IEnumerable<CountryData> getEntities{
+            get{
+                var l = new List<CountryData>();
+                foreach(CultureInfo cul in CultureInfo.GetCultures(CultureTypes.SpecificCultures)){
+                    var c = new RegionInfo(new CultureInfo(cul.Name, false).LCID);
+                    var d = createCountry(c.ThreeLetterISORegionName, c.EnglishName, c.NativeName);
+                    if (l.FirstOrDefault(x => x.Id == d.Id) is not null) continue;
+                    l.Add(d);
+                }
+                return l; }
+
+        }
+        internal static CountryData createCountry(string code, string name, string description)
+            => new(){Id = code, Code = code, Name = name, Description=description };
+    }
+}
