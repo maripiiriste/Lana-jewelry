f5ec75c4e3b3555f27e4867fc550673503649738
Mari Piiriste <mpiiri@ttu.ee>
2022-04-02 17:19:44 +0300

    mari

--- Pages/PagedPage.cs
+++ Pages/PagedPage.cs
@@ -9,8 +9,8 @@ namespace Lana_jewelry.Pages {
         where TRepo : IPagedRepo<TEntity> {
         protected PagedPage(TRepo r) : base(r) { }
 
-        public string? CurrentSort { get; set; }
-        public string? CurrentFilter { get; set; }
+        public string? CurrentSort { get; set; } = "currentSort";
+        public string? CurrentFilter { get; set; } = "currentFilter";
         public int PageIndex {
             get => repo.PageIndex; 
             set=>repo.PageIndex=value;
@@ -18,7 +18,36 @@ namespace Lana_jewelry.Pages {
         public int TotalPages => repo.TotalPages; 
         public bool HasNextPage => repo.HasNextPage;
         public bool HasPreviousPage => repo.HasPreviousPage;
-        public async override Task<ActionResult> OnGetIndexAsync(int pageIndex=0, string currentFilter=null, string sortOrder=null) {
+        public override IActionResult OnGetCreate(int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
+            PageIndex = pageIndex;
+            return base.OnGetCreate(pageIndex, currentFilter, sortOrder);
+        }
+
+        public override async Task<IActionResult> OnPostCreateAsync(int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnPostCreateAsync(pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnGetDetailsAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnGetDetailsAsync(id,pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnGetDeleteAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnGetDeleteAsync(id,pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnPostDeleteAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnPostDeleteAsync(id,pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnGetEditAsync(string id, int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnGetEditAsync(id,pageIndex, currentFilter, sortOrder);
+        }
+        public override async Task<IActionResult> OnPostEditAsync(int pageIndex = 0, string? currentFilter = null, string? sortOrder = null) {
+            PageIndex = pageIndex;
+            return await base.OnPostEditAsync(pageIndex, currentFilter?? string.Empty, sortOrder ?? string.Empty);
+        }
+        public override async Task<ActionResult> OnGetIndexAsync(int pageIndex=0, string? currentFilter =null, string? sortOrder =null) {
             PageIndex = pageIndex;
             return await base.OnGetIndexAsync(pageIndex, currentFilter, sortOrder);
         }
