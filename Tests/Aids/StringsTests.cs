6a0f9718cd3d74a9a1a2b47e7e627077c04d1b5f
Mari Piiriste <mpiiri@ttu.ee>
2022-04-17 13:24:27 +0300

    mari

--- Tests/Aids/StringsTests.cs
+++ Tests/Aids/StringsTests.cs
@@ -1,6 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Lana_jewelry.Aids;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lana_jewelry.Tests.Aids
 {
-    [TestClass] public class StringsTests : IsTypeTested { }
+    [TestClass] public class StringsTests : IsTypeTested {
+        private string testStr;
+
+        [TestInitialize] public void Init() => testStr = "a1b1c1.d1e1f1";
+        [TestMethod] public void RemoveTest() => areEqual("abc.def", Strings.Remove(testStr, "1"));
+        [TestMethod] public void IsTypeNameTest() => isInconclusive();
+        [TestMethod] public void IsTypeFullNameTest() => isInconclusive();
+        [TestMethod] public void RemoveTailTest() => areEqual("a1b1c1", Strings.RemoveTail(testStr));
+    }
 }
