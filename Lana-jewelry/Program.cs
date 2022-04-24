351be9847f0314f858d3e5cdf72946d20e0f7859
Mari Piiriste <mpiiri@ttu.ee>
2022-04-24 17:58:16 +0300

    mari

--- Lana-jewelry/Program.cs
+++ Lana-jewelry/Program.cs
@@ -13,10 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDbContext<Lana_jewelryDb>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(o => o.UseSqlServer(connectionString));
+builder.Services.AddDbContext<Lana_jewelryDb>(o => o.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
