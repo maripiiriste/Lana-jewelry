45ce84dd02cd2b74824d74c0ac64e6b8ca5ea6be
Anna Brita Ani <annani@ttu.ee>
2022-05-23 18:23:15 +0300

    Anna

--- Infra/Initializers/TransportInitializer.cs
+++ Infra/Initializers/TransportInitializer.cs
@@ -4,19 +4,23 @@ namespace Lana_jewelry.Infra.Initializers
 {
     public sealed class TransportInitializer : BaseInitializer<TransportData> {
         public TransportInitializer(Lana_jewelryDb? db) : base(db, db?.Transports) { }
-        internal static TransportData createTransport(string id, string costumerAadress, double price, DateTime duration) {
+        internal static TransportData createTransport(string id, double price, DateTime duration, string countryId, string city, string street, string zipCode) {
             var transport = new TransportData {
                 Id = id,
                 Price = price,
-                Duration = duration
+                Duration = duration,
+                CountryId = countryId,
+                City = city,
+                Street = street,
+                ZipCode = zipCode,
             };
             return transport;
         }
         protected override IEnumerable<TransportData> getEntities => new[] {
-            createTransport("HarryPotter", "Nelgi tee 37", 7, new DateTime(2022, 03, 27)),
-            createTransport("HermioneGrenger", "Aia tee 5", 5, new DateTime(2022, 03, 29)),
-            createTransport("RonaldWeasley", "Kauna 5", 15, new DateTime(2022, 03, 23)),
+            createTransport("HarryPotter", 7, new DateTime(2022, 03, 27), "Italy", "Milan", "Nelgi", "20057"),
+            createTransport("HermioneGrenger", 5, new DateTime(2022, 03, 29), "Italy", "Rome", "Kauna", "40042"),
+            createTransport("RonaldWeasley", 15, new DateTime(2022, 03, 23), "Italy", "Venice", "Nelgi", "30387"),
         };
+
     }
-   
 }
