47d15d4d2f4008526b9c612f01d672243060eeca
Anna Brita Ani <annani@ttu.ee>
2022-05-23 18:24:20 +0300

    Anna

--- Infra/Initializers/TransportShoppingBagInitializer.cs
+++ Infra/Initializers/TransportShoppingBagInitializer.cs
@@ -11,9 +11,7 @@ namespace Lana_jewelry.Infra.Initializers
     public sealed class TransportShoppingBagInitializer : BaseInitializer<TransportShoppingBagData>
     {
         public TransportShoppingBagInitializer(Lana_jewelryDb? db) : base(db, db?.TransportShoppingBags) { }
-        protected override IEnumerable<TransportShoppingBagData> getEntities
-        {
-        }
+        protected override IEnumerable<TransportShoppingBagData> getEntities => throw new NotImplementedException();
 
         internal static TransportShoppingBagData createEntity(string shoppingBagId, string transportId)
             => new()
